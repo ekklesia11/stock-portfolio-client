93948d9a78fbabeafbf6da0e6bf6ababd804c569
Daniel Park <ekklesia89@gmail.com>
2020-07-09 22:58:49 +0900

    stock api test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
+  const [code, setCode] = React.useState("");
+  const [shareInfo, setShareInfo] = React.useState({});
+
+  const getInfo = async () => {
+    const body = await axios.post("http://localhost:5000", { code: code });
+    console.log(body);
+    setShareInfo({ ...body.data });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button onClick={getInfo}>정보 가져오기</button>
+      <div>
+        <div>종목이름: {shareInfo.JongName ? shareInfo.JongName : null}</div>
+        <div>현재주가: {shareInfo.CurJuka ? shareInfo.CurJuka : null}</div>
+        <div>최고가: {shareInfo.HighJuka ? shareInfo.HighJuka : null}</div>
+        <div>최저가: {shareInfo.LowJuka ? shareInfo.LowJuka : null}</div>
+        <div>
+          금일시작가: {shareInfo.StartJuka ? shareInfo.StartJuka : null}
+        </div>
+      </div>
     </div>
   );
 }
