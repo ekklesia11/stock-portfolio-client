5b8851624dbdfefa50b92d567408aab03672b597
Daniel Park <ekklesia89@gmail.com>
2020-07-14 17:18:42 +0900

    naver juka api call added

--- src/App.js
+++ src/App.js
@@ -2,48 +2,71 @@ import React from "react";
 import axios from "axios";
 
 function App() {
-  // const [code, setCode] = React.useState("");
-  // const [shareInfo, setShareInfo] = React.useState({});
+  const [stockInfo, setStockInfo] = React.useState({});
   const [stockList, setStockList] = React.useState([]);
-
-  // const getInfo = async () => {
-  //   const body = await axios.get(`http://localhost:5000?code=${code}`);
-  //   setShareInfo({ ...body.data });
-  // };
+  const [searchText, setSearchText] = React.useState("");
+  const [selectedStock, setSelectedStock] = React.useState("");
 
   React.useEffect(() => {
     getStockList();
   }, []);
+
+  // const getStockInfo = async () => {
+  //   let code = searchFromStockList();
+
+  //   console.time("body");
+  //   const body = await axios.get(`http://localhost:5000?code=${code}`);
+  //   console.timeEnd("body");
+  //   if (body.data.CurJuka !== "") {
+  //     setStockInfo({ ...body.data });
+  //   } else {
+  //     setStockInfo({ CurJuka: "결과없음" });
+  //   }
+  // };
 
   const getStockList = async () => {
     const body = await axios.get("http://localhost:5000/code");
     setStockList([...body.data]);
   };
 
+  const searchFromStockList = () => {
+    let result = stockList.filter(
+      (v) => v.name.toLowerCase() === searchText.toLowerCase()
+    );
+    if (!result.length) {
+      setSelectedStock("종목을 찾을 수가 없습니다.");
+      return "";
+    } else {
+      setSelectedStock(`${result[0].name}: ${result[0].code}`);
+      return result[0].code;
+    }
+  };
+
+  const enterSearch = (e) => {
+    if (e.key === "Enter") {
+      // getStockInfo();
+      naverSearch();
+    }
+  };
+
+  const naverSearch = async () => {
+    let code = searchFromStockList();
+    const body = await axios.get(`http://localhost:5000/?code=${code}`);
+    setStockInfo({ ...body.data });
+  };
+
   return (
     <div>
-      {/* <input
+      <input
         type="text"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
+        value={searchText}
+        onKeyPress={enterSearch}
+        onChange={(e) => setSearchText(e.target.value)}
       />
-      <button onClick={getInfo}>정보 가져오기</button>
-      <div>
-        <div>종목이름: {shareInfo.JongName ? shareInfo.JongName : null}</div>
-        <div>현재주가: {shareInfo.CurJuka ? shareInfo.CurJuka : null}</div>
-        <div>최고가: {shareInfo.HighJuka ? shareInfo.HighJuka : null}</div>
-        <div>최저가: {shareInfo.LowJuka ? shareInfo.LowJuka : null}</div>
-        <div>
-          금일시작가: {shareInfo.StartJuka ? shareInfo.StartJuka : null}
-        </div>
-      </div> */}
-      <select>
-        {stockList.map((v) => (
-          <option key={v.code}>
-            {v.name}: {v.code}
-          </option>
-        ))}
-      </select>
+      {/* <button onClick={getStockInfo}>검색</button> */}
+      <button onClick={naverSearch}>검색</button>
+      <h1>{selectedStock}</h1>
+      <h2>현재가: {stockInfo.now}</h2>
     </div>
   );
 }
