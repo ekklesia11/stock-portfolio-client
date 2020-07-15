c7520b3a11d97e3f991618f12abbafe2844821bb
Daniel Park <ekklesia89@gmail.com>
2020-07-15 23:36:55 +0900

    kakao and google login button added

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,23 +1,40 @@
 import React from "react";
+
+import SocialBtns from "./socialBtns";
+
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 
 const Login = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <div>
-        <label>email</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>password</label>
-        <input type="text" />
-      </div>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField id="standard-basic" label="Email" />
+        </div>
+        <div>
+          <TextField id="standard-basic" label="Password" />
+        </div>
+      </form>
       <div>
         <Button variant="contained" color="primary">
           sign in
         </Button>
         <Button color="primary">sign up</Button>
       </div>
+      <SocialBtns />
     </div>
   );
 };
--- src/components/login/socialBtns.js
+++ src/components/login/socialBtns.js
@@ -1,8 +1,20 @@
 import React from "react";
 import axios from "axios";
 
+import kakaoLoginBtn from "../../assets/kakao_login_medium_narrow.png";
+import googleLoginBtn from "../../assets/btn_google_signin_light_normal_web.png";
+
 const SocialBtns = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src={kakaoLoginBtn} alt="" />
+      </div>
+      <div>
+        <img src={googleLoginBtn} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default SocialBtns;
