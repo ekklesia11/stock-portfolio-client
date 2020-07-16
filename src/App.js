78a74c718891f9ce294a4f7fb48d7a3aaa6e6759
Daniel Park <ekklesia89@gmail.com>
2020-07-16 22:10:22 +0900

    react router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import CurStock from "./components/temp/curStock";
 import Login from "./components/login/login";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <nav>
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+        <div>
+          <Link to="/search">Search</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/search">
+          <CurStock />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
