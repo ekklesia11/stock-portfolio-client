f8d893e3bcc209a7fcdcb32cc64cdc1ce0f4d755
Daniel Park <ekklesia89@gmail.com>
2020-07-16 23:50:21 +0900

    social login btns updated

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,38 +2,9 @@ import React from "react";
 
 import SocialBtns from "./socialBtns";
 
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-}));
-
 const Login = () => {
-  const classes = useStyles();
-
   return (
     <div>
-      <form className={classes.root} noValidate autoComplete="off">
-        <div>
-          <TextField id="standard-basic" label="Email" />
-        </div>
-        <div>
-          <TextField id="standard-basic" label="Password" />
-        </div>
-      </form>
-      <div>
-        <Button variant="contained" color="primary">
-          sign in
-        </Button>
-        <Button color="primary">sign up</Button>
-      </div>
       <SocialBtns />
     </div>
   );
--- src/components/login/socialBtns.js
+++ src/components/login/socialBtns.js
@@ -1,5 +1,7 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
+import styled from "styled-components";
+import Link from "@material-ui/core/Link";
 
 import kakaoLoginBtn from "../../assets/kakao_login_medium_narrow.png";
 import googleLoginBtn from "../../assets/btn_google_signin_light_normal_web.png";
@@ -11,10 +13,27 @@ const SocialBtns = () => {
         <img src={kakaoLoginBtn} alt="" />
       </div>
       <div>
+        <NaverLogin>네이버 로그인</NaverLogin>
+      </div>
+      <div>
         <img src={googleLoginBtn} alt="" />
       </div>
+      <Link href="#" color="primary">
+        문의하기
+      </Link>
     </div>
   );
 };
+
+const NaverLogin = styled.div`
+  width: 180px;
+  height: 45px;
+  background-color: #19ce60;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
 
 export default SocialBtns;
