115b10773bcf795e663e2d14975c72cb5390226d
Daniel Park <ekklesia89@gmail.com>
2020-07-29 21:43:34 +0900

    global css style added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import CurStock from "./components/temp/curStock";
 import Login from "./components/login/login";
+import Layout from "./components/layout/layout";
+import Main from "./components/main/main";
+
+const GlobalStyle = createGlobalStyle({
+  body: {
+    backgroundColor: "#f3f4f7",
+    padding: 0,
+    margin: 0,
+  },
+});
 
 function App() {
   return (
     <Router>
-      <nav>
-        <div>
-          <Link to="/login">Login</Link>
-        </div>
-        <div>
-          <Link to="/search">Search</Link>
-        </div>
-      </nav>
-
+      <GlobalStyle />
       <Switch>
+        <Route exact path="/">
+          <Layout>
+            <Main />
+          </Layout>
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
