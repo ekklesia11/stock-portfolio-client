956935fcaad95d5b090fa7cfdf8afc958462a532
Daniel Park <ekklesia89@gmail.com>
2020-08-07 00:29:54 +0900

    react redux installed and store created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import stockApp from "./redux/reducers";
 import App from "./App";
 
+const store = createStore(stockApp);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
