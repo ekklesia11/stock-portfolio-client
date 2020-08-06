7f8f241d6a064638651ae5ce210367799dd92844
Daniel Park <ekklesia89@gmail.com>
2020-08-06 23:19:24 +0900

    valuation components added

---
+++ src/components/valuation/pbrBase.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+
+const PbrBase = () => {
+  const [eps, setEps] = useState("");
+  const [per, setPer] = useState("");
+  const [perGroup, setPerGroup] = useState([]);
+  const [epsPerResult, setEpsPerResult] = useState("");
+  const [avgPer, setAvgPer] = useState(0);
+
+  useEffect(() => {
+    calculateAvgPer(perGroup);
+  }, [perGroup]);
+
+  const calculateAvgPer = (pers) => {
+    if (pers.length) {
+      let avg = pers.reduce((acc, cur) => acc + Number(cur), 0) / pers.length;
+      setAvgPer(avg.toFixed(2));
+    }
+  };
+
+  const addPer = () => {
+    if (per !== "") {
+      setPerGroup((prev) => [...prev, per]);
+      setPer("");
+    } else {
+      alert("추가할 PER 정보가 없습니다!");
+    }
+  };
+
+  const reset = () => {
+    setEps("");
+    setPer("");
+    setPerGroup((prev) => []);
+    setEpsPerResult("");
+    setAvgPer(0);
+  };
+
+  return (
+    <div>
+      <fieldset>
+        <legend>
+          <h2>2. BPS x 평균 PBR</h2>
+        </legend>
+        <label>BPS: </label>
+        <input
+          type="text"
+          value={eps}
+          onChange={(e) =>
+            Number(e.target.value)
+              ? setEps(e.target.value)
+              : alert("숫자를 입력하세요")
+          }
+        />
+        <br />
+        <br />
+        <label>PBR: </label>
+        {avgPer !== "" ? <strong>평균 {avgPer}</strong> : null}
+        <ul>
+          {perGroup.map((per, i) => (
+            <li key={i}>{per}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={per ? per : ""}
+          onChange={(e) =>
+            Number(e.target.value) >= 0 || e.target.value === ""
+              ? setPer(e.target.value)
+              : alert("숫자를 입력하세요")
+          }
+          onKeyUp={(e) => (e.keyCode === 13 ? addPer() : null)}
+        />
+        <button onClick={addPer}>PBR 추가</button>
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            setEpsPerResult((avgPer * Number(eps)).toFixed(2));
+          }}
+        >
+          계산
+        </button>
+        <button onClick={reset}>초기화</button>
+        <br />
+        <br />
+        <label>결과: </label>
+        <h1>{epsPerResult}</h1>
+      </fieldset>
+    </div>
+  );
+};
+
+export default PbrBase;
--- src/components/valuation/valuation.js
+++ src/components/valuation/valuation.js
@@ -1,86 +1,18 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 
 // EPS * avg PER(5 years)
 // BPS * avg PBR(5 years)
 // EPS * ROE(percentage)
+import PerBase from "./perBase";
+import PbrBase from "./pbrBase";
+import RoeBase from "./roeBase";
+
 const Valuation = () => {
-  const [eps, setEps] = useState("");
-  const [per, setPer] = useState("");
-  const [perGroup, setPerGroup] = useState([]);
-  const [epsPerResult, setEpsPerResult] = useState("");
-  const [avgPer, setAvgPer] = useState(0);
-
-  useEffect(() => {
-    calculateAvgPer(perGroup);
-  }, [perGroup]);
-
-  const calculateAvgPer = (pers) => {
-    if (pers.length) {
-      let avg = pers.reduce((acc, cur) => acc + Number(cur), 0) / pers.length;
-      setAvgPer(avg.toFixed(2));
-    }
-  };
-
-  const addPer = () => {
-    if (per !== "") {
-      setPerGroup((prev) => [...prev, per]);
-      setPer("");
-    } else {
-      alert("추가할 PER 정보가 없습니다!");
-    }
-  };
-
   return (
     <div>
-      <fieldset>
-        <legend>
-          <h2>1. EPS x 5년간 평균 PER</h2>
-        </legend>
-        <label>EPS: </label>
-        <input
-          type="text"
-          value={eps}
-          onChange={(e) =>
-            Number(e.target.value)
-              ? setEps(e.target.value)
-              : alert("숫자를 입력하세요")
-          }
-        />
-        <br />
-        <br />
-        <label>PER: </label>
-        {avgPer !== "" ? <strong>평균 {avgPer}</strong> : null}
-        <ul>
-          {perGroup.map((per, i) => (
-            <li key={i}>{per}</li>
-          ))}
-        </ul>
-        <input
-          type="text"
-          value={per ? per : ""}
-          onChange={(e) =>
-            Number(e.target.value) || e.target.value === ""
-              ? setPer(e.target.value)
-              : alert("숫자를 입력하세요")
-          }
-          onKeyUp={(e) => (e.keyCode === 13 ? addPer() : null)}
-        />
-        <button onClick={addPer}>PER 추가</button>
-        <br />
-        <br />
-        <button
-          onClick={() => {
-            setEpsPerResult((avgPer * Number(eps)).toFixed(2));
-          }}
-        >
-          계산
-        </button>
-        <br />
-        <br />
-        <label>결과: </label>
-        <h1>{epsPerResult}</h1>
-      </fieldset>
+      <PerBase />
+      <PbrBase />
+      <RoeBase />
     </div>
   );
 };
