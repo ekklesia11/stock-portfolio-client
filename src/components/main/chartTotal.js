a9b32091962d1113cc68c750b8db81159932615f
Daniel Park <ekklesia89@gmail.com>
2020-08-14 12:22:26 +0900

    two pie chart added / chart layout updated / table removed

--- src/components/main/chartTotal.js
+++ src/components/main/chartTotal.js
@@ -4,21 +4,30 @@ import styled from "styled-components";
 import PieDiagram from "./pieDiagram";
 import BarDiagram from "./barDiagram";
 
-const ChartLayout = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
 const ChartTotal = () => {
   return (
     <ChartLayout>
-      <PieDiagram />
-      <div>
+      <div className="pie">
+        <PieDiagram />
+        <PieDiagram />
+      </div>
+      <div className="bar">
         <BarDiagram />
         <BarDiagram />
       </div>
     </ChartLayout>
   );
 };
+
+const ChartLayout = styled.div`
+  .pie {
+    display: flex;
+    justify-content: center;
+  }
+  .bar {
+    display: flex;
+    justify-content: center;
+  }
+`;
 
 export default ChartTotal;
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -11,7 +11,6 @@ const Main = () => {
       <CashTotal />
       <StockTotal />
       <ChartTotal />
-      <StockTable />
     </div>
   );
 };
